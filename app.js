3ba13685ec2b459238f48a70125e5a73ffa18859
Roy Leong <l3ongroy@gmail.com>
2020-10-21 22:43:03 +0800

    Local server with Atlas DB

--- app.js
+++ app.js
@@ -1,26 +1,48 @@
-//jshint esversion:6
-var _ = require('lodash');
+//    Constant Declarations
+const _ = require('lodash');
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta.t amet, consectetur adipiscing elit. Integer a venenatis risus. Duis ac aliquam dui. Mauris vulputate odio magna. Mauris fermentum vehicula justo vel convallis. Etiam sed porta erat. Vivamus porttitor libero nec leo auctor sodales. Sed nec erat ut diam commodo dapibus nec nec enim. Integer gravida bibendum elit, a ultricies dolor molestie id. Vestibulum ac commodo est. Duis suscipit vel ligula in pretium. Cras in purus vitae massa efficitur luctus at sit amet dolor.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. ";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat.";
 
 const app = express();
-let posts = [];
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+let posts = [];
+
+//    Database 
+mongoose.connect('mongodb+srv://admin-roy:Royl3ong@cluster0.woyal.mongodb.net/blogDB?retryWrites=true&w=majority', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model('Post', postSchema);
+
+//    Get functions
 
 app.get('/', function (req, res) {
-  res.render('home', {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+  
+  Post.find({}, function(err, foundPosts) {
+    if (!err) {
+      
+      res.render('home', {
+        homeStartingContent: homeStartingContent,
+        posts: foundPosts
+      });
+    }
   });
+  
 });
 
 app.get('/about', (req, res) => {
@@ -40,7 +62,25 @@ app.get('/compose', (req, res) => {
 });
 
 app.get('/posts/:postName', (req, res) => {
-  let postName = _.lowerCase(req.params.postName);
+  // let postName = _.lowerCase(req.params.postName);
+  let postName = req.params.postName;
+  
+  Post.findOne({title: postName}, function(err, foundPost) {
+    if (!err) {
+      
+      if (!foundPost) {
+        res.redirect('/');
+      } 
+      
+      else {  
+        res.render('post', {
+          title: foundPost.title,
+          body: foundPost.content
+        });
+      }
+    }
+  });
+  
   
   for (i of posts) {
     let lowerCasePostName = _.lowerCase(i.title);
@@ -54,23 +94,40 @@ app.get('/posts/:postName', (req, res) => {
   
 });
 
+
+//    Post Functions
+
 app.post('/compose', (req, res) => {
   const newTitle = req.body.newTitle;
   const newBody = req.body.newBody;
-  const postData = {
+  const postData = new Post ({
     title: newTitle,
-    body: newBody
-  };
-  posts.push(postData);
+    content: newBody
+  });
+  
+  postData.save();
   res.redirect('/');
+});
+
+app.post('/delete', function(req, res) {
+  const postTitle = req.body.postTitle;
+  
+  Post.findOneAndDelete({title: postTitle}, function(err, deletedPost) {
+    if (!err) {
+      console.log('Successfully deleted ' + deletedPost.title);
+      res.redirect('/');
+    }  
+  });
 });
 
 
 
+//    Listen to port
 
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port,function() {
+  console.log('Server running');
 });
